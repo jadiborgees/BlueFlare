f1768b0af9d54ef4be5923aebc0315a6075d311f
Jadi Borges <jadiborgees@gmail.com>
2026-06-21 20:57:05 -0300

    Remove pagina de produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
-import { Produtos } from './pages/produtos/produtos';
 import { QuemSomos } from './pages/quem-somos/quem-somos';
 import { Contato } from './pages/contato/contato';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'produtos', component: Produtos },
   { path: 'quem-somos', component: QuemSomos },
   { path: 'contato', component: Contato },
   { path: '**', redirectTo: '' }
